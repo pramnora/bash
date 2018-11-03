488046aec4e14d4d6f271ae4faac813715d467d0
Paul Ramnora <pramnora@yahoo.com>
2018-11-03 21:08:20 +0000

    Create set-password.sh
    Created: password.sh...allows one to change the Raspberry pi <user> password to anything you please.

---
+++ set-password.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#*** Script allows you to set/reset the <username> password on the Raspbian Pi to anything you please;
+#    where <username> in this particular case is called: <root>
+sudo passwd root
