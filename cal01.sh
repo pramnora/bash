b354e612398e75ecbe34f8a8f6c225bce46b5cce
Paul Ramnora <pramnora@yahoo.com>
2018-11-09 05:30:19 +0000

    Create cal01.sh
    Created file: cal02.sh...shows how to display the calender

---
+++ cal01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# shows the calender for the current month...
+cal
+
+# shows the calender for 3 months: 1 month behind/the current month/1 month in front
+cal -3
+
+# shows the calender for the date April (...4th month/year:) 1963...
+cal 4 1963
