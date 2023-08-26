beac54f522b18ddee2eb2bfe08cc2b4819e72c8d
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 12:06:47 +0100

    Update array01.sh
    Added some vertical spacing/and, also, extended commentary text

--- language/data-structures/arrays/array01.sh
+++ language/data-structures/arrays/array01.sh
@@ -4,13 +4,17 @@ names=("Tom" "Dick" "Harry")
 
 echo Show all array items...
 echo "${names[@]}"
-echo Show an individual item by slected array subscript[0]...
+
+echo Show an individual item by selected array subscript number[0]...
 echo "${names[0]}"
-echo Change an indivdual array subscript data...subscript[0]...
+
+echo Change an individual array subscript data...by using its subscript number[0]...; 
 names[0]='Jack'
 echo "${names[@]}"
+
 echo Show each array item subscript...
 echo "${!names[@]}"
+
 echo Show the total number of array items...
 echo "${#names[@]}"
 
