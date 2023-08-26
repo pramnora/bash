d7980657e9ad96fd15b20e1d3525bafa3fd24e61
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 18:11:19 +0100

    Create create-folder01.sh
    Created file: [create-folder01.sh] Demo, shows how to create a folder using the mkdir command

---
+++ language/filing/create-folder01.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash  
+
+# create a new directory folder  
+
+mkdir folderName1  
+
+# a 'new' directory folder is created called: folderName1  
+
+# NOTE: You can also create multiple folders all together at once by using...
+#       mkdir folderName1 folderName2 folderName3
+#       -etc.
