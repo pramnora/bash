4949abbfbe650214aab47e73bd03cd53978c875c
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 18:15:01 +0100

    Create remove-file01.sh
    Created file: [remove-file01.sh] Demo, how to remove a name file...using the 'rm' command

---
+++ language/filing/remove-file01.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# remove a file called: fileName1...
+
+rm fileName1
+
+# NOTE: It's possible to remove multiple files, as well...; use...
+#       rm fileName1 fileName2 fileName3
+#       -etc.
