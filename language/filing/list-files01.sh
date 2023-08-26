93cfd370a1b947a935868dd22daf140171127cdf
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 19:16:41 +0100

    Update list-files01.sh
    Added another note

--- language/filing/list-files01.sh
+++ language/filing/list-files01.sh
@@ -4,11 +4,17 @@
 
 ls
 
-# NOTE(1): NOT 'all' folders/files will be listed here...; 
+#NOTE(1): The long form of listing folders/files is to use:    
+#         ls -l  
+#         ...this will show not just the folder names; but, also, many other details such as:    
+#         file permissions dates times size   
+#         -etc.  
+
+# NOTE(2): NOT 'all' folders/files will be listed here...; 
 #          such as files containing a prefix dot: . are 'hidden' system files;  
 #          in order to list all folders/files use:   
 #          ls -al  
 
-# NOTE(2): It's also possible to list folders/files inside of a different folder directory, as well; use...;  
+# NOTE(3): It's also possible to list folders/files inside of a different folder directory, as well; use...;  
 #          ls folderName  
 #          ...would list all folders/files inside of the 'named' folder directory.  
