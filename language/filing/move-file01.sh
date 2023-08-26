a6b2389f48668827200f3914f48b11de41e36df9
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 17:40:29 +0100

    Create move-file01.sh
    Created file: [move-file01.sh] Demo, shows how to move a file from one directory to a next

---
+++ language/filing/move-file01.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# move a file from the current folder directory; and, into a different folder directory...
+# assuming file structure:-
+# current folder contains -> fileName1
+# sub folder: test -> empty
+
+move fileName1 test
+
+# after this command...
+# current folder contains -> empty
+# sub folder: test -> fileName1
