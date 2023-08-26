d3860f0b9024a30aad490630f97d0befd01623c5
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 12:08:08 +0100

    Update array01.sh
    Added commentary text

--- language/data-structures/arrays/array01.sh
+++ language/data-structures/arrays/array01.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-names=("Tom" "Dick" "Harry")
+names=("Tom" "Dick" "Harry") # create an array to store names
 
 echo Show all array items...
 echo "${names[@]}"
@@ -12,7 +12,7 @@ echo Change an individual array subscript data...by using its subscript number[0
 names[0]='Jack'
 echo "${names[@]}"
 
-echo Show each array item subscript...
+echo Show each array item subscript number...
 echo "${!names[@]}"
 
 echo Show the total number of array items...
