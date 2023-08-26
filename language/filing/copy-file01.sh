5a6a11fb8b28ce4c881c5a43679bce709d67c796
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 17:30:39 +0100

    Create copy-file01.sh
    Created file: [copy-file01.sh] Demonstrates how to copy files

---
+++ language/filing/copy-file01.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# copies the contents of fileName1; and, stores it inside of a new file called: fileName2
+
+cp fileName1 fileName2
+
+# NOTE: Linux filenames do NOT need to use any filename extension;
+#       but, you can if you wish add one...;
+#       especially, if you want to share files with a different OS/Operating System such as Windows
