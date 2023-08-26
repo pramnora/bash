6180742b7314432eeace6dd7f378963b7995ee02
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 19:48:12 +0100

    Create funArgs01.sh
    Created file: [funArgs01.sh] Demo's how to create a function which allows the user to pass in parameter arguments

---
+++ language/functions/funArgs01.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# create a function called: print()...
+
+function print(){ 
+ # this function accepts 3 x parameter arguments... 
+ echo $1 $2 $3
+}
+print Hi John Smith
+
+# NOTE: One calls the function without using the pair of circular brackets: ()
