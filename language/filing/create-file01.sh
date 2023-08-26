ab9d968603d7c745219e353b3e20a431b8f08878
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 17:45:30 +0100

    Create create-file01.sh
    Create file: [create-file01.sh] Demo, how to create a new file by using the touch command

---
+++ language/filing/create-file01.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# create a new file called fileName1
+
+touch fileName1
+
+# NOTE: In Linux filenames are 'case sensitive'...; 
+#       meaning, filename, fileName, FILENAME
+#       are NOT the same; but, are 3 separate files, instead
