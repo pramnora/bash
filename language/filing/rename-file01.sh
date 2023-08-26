8e5905437a231c35c32869357c5780f1eed8c643
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 17:35:56 +0100

    Create rename-file01.sh
    Created file: [rename-file01.sh] Demo, how to use the mv command to rename a file

---
+++ language/filing/rename-file01.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# rename a file using the mv/or, move command...
+# Here, fileName1 is renamed to become fileName2
+ 
+mv fileName1 fileName2
+
+# NOTE: The mv command has 2 different modes...
+# 1> It moves a file from one place to another
+# 2> but, it can also be used to rename files, as well
