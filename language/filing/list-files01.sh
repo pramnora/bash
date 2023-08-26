f33297b8f01ff1d35e2c1a885cd389bab046b39c
Paul Ramnora <pramnora@yahoo.com>
2023-08-26 17:50:01 +0100

    Create list-files01.sh
    Create file: [list-files01.sh] Lists files inside of the current folder...by using the ls command

---
+++ language/filing/list-files01.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# list files inside of the current folder directory...
+
+ls
+
+# NOTE: NOT 'all' files will be listed here...; 
+#       such as files containing a prefix dot: . are 'hidden' system files;  
+#       in order to list all files use: ls -al
