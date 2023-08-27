2686bf993fb3a1c9999a12716058b2391e59102f
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 12:45:02 +0100

    Create heredoc01.sh
    Created file: [heredoc01.sh] Demo, how create heredoc...which can be both shown/read from within the program itself.

---
+++ language/comments/heredoc01.sh
@@ -0,0 +1,14 @@
+cat << heredoc
+This is some heredoc text other people can see/read when interacting
+with a bash script program.
+
+NOTE: The word: 'heredoc' both precedes/and, also, 'ends' the file;
+      however, you could also have chosen to use any other word such as
+      to replace the word: 'heredoc'...with...
+      'eof'/meaning, 'end of file'...;
+      or else, something perfectly meaningless like: 'xyz';
+      /-etc.
+      Ultimately, which word to choose to use to begin/end, 
+      is left entirely up to the programmer themselves. 
+heredoc
+
