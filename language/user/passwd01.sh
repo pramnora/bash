07f1072d587c55bdef49e17e1aec3f4b9a617920
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 23:28:04 +0100

    Create passwd01.sh
    Created file: [passwd01.sh] Shows how to change what is the current user's password...by using the command: passwd

---
+++ language/user/passwd01.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash  
+
+# re-set password for current user...  
+
+passwd  
+
+# it will prompt you for what is your current password...;  
+# and, then, if you type it in correctly...;  
+# it will prompt you to enter the new password, twice...in order to confirm.  
