894f1f941789fd3027370b4df57f9ff4c11892fb
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 22:36:10 +0100

    Create clear01.sh
    Created file: [clear01.sh] Demo, how to clear the terminal screen...by using the 'clear' command.

---
+++ language/clear01.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+clear # clear's the terminal screen.../which is similar to CLS inside of Windows CLI/Command Line Interface Prompt
+
+# NOTE: If you continue scrolling up and down...by using your arrow keys...;   
+# then, you will see that all of the previous commands you issued/plus, output still remains there...;
+# and, it did NOT, quite simply, vanish.
