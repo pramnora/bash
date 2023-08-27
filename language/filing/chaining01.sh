4a910201e5230bddef01cee8fd167730b5e63ce7
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 12:55:08 +0100

    Create  chaining01.sh
    Created file: [chaining01.sh] Demo, how to chain multiple commands by using '&&'

---
+++ language/filing/chaining01.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Shows how to chain commands together using: '&&'...  
+
+# NOTE: This example assumes that 'fileName' already exists inside of the current directory folder...   
+
+mkdir test && mv fileName
+
