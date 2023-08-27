25a5b6d35af56bb34c5c3874acf1073d53ecc9f2
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 23:36:19 +0100

    Create su01.sh
    Created file: [su01.sh] The command: su username, allows one to 'switch user'...by switching who is the current user of the system. NOTE: Type 'exit' to return back to who was the previously signed in user.

---
+++ language/user/su01.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash  
+
+# switch who is the current user command...  
+
+su username
+
+# ...issuing this command, will change whoever is signed in as being the current user of the system;  
+# in order to get out...just type...
+# exit
+# ...which will return you back to what was the previously signed in user.
+
+
