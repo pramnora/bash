0790aa0fc4cc33008b0507315003e8b4091474ee
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 23:40:03 +0100

    Update su01.sh
    Added some further descriptive text...such as: su -/-etc.

--- language/user/su01.sh
+++ language/user/su01.sh
@@ -4,9 +4,16 @@
 
 su username
 
-# ...issuing this command, will change whoever is signed in as being the current user of the system;  
-# in order to get out...just type...
+# ...issuing this command, will change whoever is signed in as being the current user of the system;    
+# it will, also, ask you to type in what is that username's password to confirm who is signing in.  
+# NOTE: In order to get out...just type...
 # exit
 # ...which will return you back to what was the previously signed in user.
+
+# A next variation of this command is...  
+# su -
+# ...which will sign in the 'root' user;   
+# it will prompt you to enter what is the root users password, of course;  
+# before letting you in as root.    
 
 
