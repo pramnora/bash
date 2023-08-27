8ee10c9ffb34af8c4c1281633e77528a077e03fd
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 13:37:33 +0100

    Create math-ops01.sh
    Created: [maths-ops01.sh] Demo, shows how maths operators are used, including: addiction/subtraction/multiplication/division/exponentiation

---
+++ language/maths/math-ops01.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# Shows how to maths ops are used...
+
+echo $((3+3)) # 6/addition
+echo $((3-3)) # 0/subtraction
+echo $((3*3)) # 9/multiplication 
+echo $((3/3)) # 1/division
+echo $((3**3)) # 27/exponent
+
+
