6d098952aec9b15aaf3b48b9f0a94bc14e8769c7
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 12:42:59 +0100

    Create comments01.sh
    Created file: [comments01.sh] Demo, how to use both single line/and, too, multi-line comments

---
+++ language/comments/comments01.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# This is a single line comment.
+# NOTE: A single line comment beings with a hash: (#) symbol.
+
+: ' This is a multi-line comment block.
+
+    NOTE: A multi-line comment block starts with a colon symbol (:)
+          which is placed at the very beginning of the line,
+	  and, inside of the first column;
+	  this, colon symbol is next followed by a single space,
+	  and, a single apostrophe symbol.
+
+	  The multi-line comment block is, finally, closed off
+	  by using a single apostrope symbol.  
+'
+
+echo Comments file
