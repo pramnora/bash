f81de4fa57291cc526815336b9b5078873e5f08a
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 22:38:28 +0100

    Update clear01.sh
    Added NOTE(2)...how to clear the Terminal screen...by using keyboard shortcut: [CTRL] + [L]

--- language/terminal/clear01.sh
+++ language/terminal/clear01.sh
@@ -2,6 +2,11 @@
 
 clear # clear's the terminal screen.../which is similar to CLS inside of Windows CLI/Command Line Interface Prompt
 
-# NOTE: If you continue scrolling up and down...by using your arrow keys...;   
+# ------------------------------------------------------------------------------
+
+# NOTE(1): If you continue scrolling up and down...by using your arrow keys...;   
 # then, you will see that all of the previous commands you issued/plus, output still remains there...;
 # and, it did NOT, quite simply, vanish.
+
+
+# NOTE(2): Another to clear the terminal screen is using keyboard shortcut: [CTRL] + [L]
