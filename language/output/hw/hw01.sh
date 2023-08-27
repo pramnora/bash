9180480bb172be97dfebc29bf863456cee65f109
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 14:01:23 +0100

    Update hw01.sh
    Updated file with both Example 2/Example 3

--- language/output/hw/hw01.sh
+++ language/output/hw/hw01.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/bash
-# NOTE: Hello, world!...does NOT need any quote marks around it...
+
+# Example 1...  
+
+# NOTE: Hello, world!...does NOT need any quote marks around it: ("Hello, world!")...
 echo Hello, world!
+
+# Example 2...  
+
+echo What\'s up in the \'ouse # NOTE: Single quote marks need to be escaped by using the back slash symbol: (\); otherwise, the single quote marks would NOT appear inside of final output
+
+# Example 3...
+
+echo "What's up in the 'ouse" # If you wish to use single quotes:(')...; then, you can choose to surround the whole sentence inside a pair of double quote marks(""), instead. 
+
+
