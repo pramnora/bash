3c4b6d2adc1016a4c860a2d4b1a9142657b8c395
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 23:08:47 +0100

    Update history01.sh
    Added another paragraph of explanatory comment.

--- language/terminal/history01.sh
+++ language/terminal/history01.sh
@@ -3,4 +3,7 @@
 # Shows the previous history list of all terminal commands that the user had typed in...;  
 # these include both during the 'previous/current' sessions.  
 
+# -(So, if you close down a session...; by closing the CLI window...;    
+#   the history of all your previous commands does still remain there.)-  
+
 history  
