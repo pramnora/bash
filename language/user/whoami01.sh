463749ad0ed34a88b040c496afcbfee9c6f9a79f
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 23:31:52 +0100

    Create whoami01.sh
    Created file: [whoami01.sh] Shows who is the currently signed in user...that the session applies to. Type in command: whoami

---
+++ language/user/whoami01.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Discover who is the currently signed in user...  
+
+whoami
+
+# will reply by stating who is the, currently, signed in user...;  
+# in my own case, I got back the message...   
+# admin3  
