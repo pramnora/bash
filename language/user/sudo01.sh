c2ebf76a815444cd853cbb3977e7936d4a56c6eb
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 23:53:21 +0100

    Create sudo01.sh
    Created file: [sudo.sh] Explaining the use of the 'sudo'/super user do command.

---
+++ language/user/sudo01.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash  
+
+# sudo/super user do...  
+
+sudo cd /home/user  
+
+# ...when you prefix any command with: sudo...;  
+# you will be requested to type in a user password to confirm;  
+# then, this means you have extra privileges/powers which you can use...;  
+# for example, a normal ordinary user is NOT allowed to: 
+# edit/delete/view certain user a/c files/nor update the system/install software/-etc.;    
+# not unless they have special 'sudo' account status.  
+
+# -( Sudo account status is conferred by whoever happens to be the 'root' user of the system;  
+#    by adding that username to the sudo file.)- 
