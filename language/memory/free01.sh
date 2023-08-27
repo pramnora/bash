b4985863e260929bfd20b95b8b48d220d10299bf
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 13:10:12 +0100

    Create free01.sh
    Created: [free01.sh] Demo, shows how to view the system memory...by using: free

---
+++ language/memory/free01.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# show how much memory is currently available to use on the computer system...
+
+free
+
+# NOTE: The free command uses certain flags...
+
+free -b # shows the available memory in bytes
+free -m # shows the available memory in megabytes
+free -g # shows the available memory in gigabytes
