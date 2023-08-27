6542784f6cbbc01fdbbe791dd63055ab90ca1338
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 22:25:59 +0100

    Create shells01.sh
    Created file: [shells.sh] Shows the list of all shells that can by used on the current system.

---
+++ language/system/vars/shells01.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+cat /etc/shells
+
+: '
+NOTE(1): The first line of a bash shell script is the above;
+         which declares where on the computer system 
+          a particular shell can be found...
+          ...to use to do bash shell scripting with.
+
+ NOTE(2): Be careful to spell: '/usr/'...; 
+          and, not write it as: '/user/'...
+	  which would be a wrong misspelling...that produces an error message?!
+'
