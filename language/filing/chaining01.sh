0827863b48d527e64fe80bd79e87d4aa26d010ce
Paul Ramnora <pramnora@yahoo.com>
2023-08-27 12:59:37 +0100

    Update chaining01.sh
    Added example 2 code...

--- language/filing/chaining01.sh
+++ language/filing/chaining01.sh
@@ -6,3 +6,14 @@
 
 mkdir test && mv fileName
 
+#---------------------------------------------------------------------------------
+
+# Another example would be...
+
+touch fileName2 && mkdir test2 && mv fileName2 test2
+
+# in this case, 
+# the file: fileName2, is created
+# the folder: test 2, is created
+# the file: fileName2 is moved into the newly created test2 folder
+
