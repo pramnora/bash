bc7a09537141e95afc8484d4a90bf7fbcdadfe1d
Paul Ramnora <pramnora@yahoo.com>
2023-08-28 18:55:27 +0100

    Update brace-expansion01.sh
    Updated file with 2 more variations...counting backwards: 10-1/Z-A

--- language/regexp/brace-expansion01.sh
+++ language/regexp/brace-expansion01.sh
@@ -3,9 +3,11 @@
 # regexp/regualar expressions: brace expansion...  
 
 echo {0..9} # 0 1 2 3 4 5 6 7 8 9  
+echo {10..1} # 10 9 8 7 6 5 4 3 2 1
 echo {65..90} # 65 66 67 68 69 70 71 72 73 74 75 76 78 79 80 81 82 83 84 85 86 87 88 89 90
 echo {a..z} # a b c d e f g h i j k l m n o p q r s t u v w x y z  
-echo {A..Z} # A B C D E F G H I J K L M N O P Q R S T U V W X Y Z  
+echo {A..Z} # A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+echo {Z..A} # Z Y X W V U T S R Q P O N M L K J I H G F E D C B A  
 echo {a..e} {A..E} # a b c d e A B C D E  
 
 # create files...
