50697c521cd5c610e8d0379f19a3d543f91c1158
Paul Ramnora <pramnora@yahoo.com>
2023-08-28 10:05:27 +0100

    Create find01.sh
    Created file: [find01.sh] Shows how to use the 'find' command...; and, gives a number of variations on it's usage...; as well as, how to find manual help in order to learn more.

---
+++ language/system/search/find01.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# The find command can be used to find where a file/directory is on the system...  
+
+find fileName
+
+# If the directory folder is found...; then, the find command will also list all of the files it contains...  
+
+find dirName 
+
+# In order to learn more about how to use find...; type in...  
+man find 
