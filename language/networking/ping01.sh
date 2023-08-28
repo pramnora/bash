8ddc9f72cf5e5a2a2fb76f5ef893e5918555e053
Paul Ramnora <pramnora@yahoo.com>
2023-08-28 01:35:23 +0100

    Create ping01.sh
    Created file: [ping01.sh] Tells you how to use the 'ping' command...to test if a remote computer is responding/or, not...?

---
+++ language/networking/ping01.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash  
+
+# ping URLAddress, tells you whether a remote computer is responsive or not...
+
+ping yahoo.com
+
+# If the 'ping' command is working, correctly; then, you should get back some response packets...
+    
+# NOTE: Press [CTRL] + [C] to cancel receiving any further packets of info.;    
+# otherwise, you might find yourself stuck inside of an 'endless loop'...?!  
