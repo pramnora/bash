a9f7dcb3cc24d550be650c06b316695e568e2656
Paul Ramnora <pramnora@yahoo.com>
2023-08-28 10:47:52 +0100

    Create add03.sh
    Created file: [add03.sh] Demo, how to add up numbers by using 2 command line arguments.

---
+++ language/maths/add/add03.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash  
+echo $(($1+$2))
+
+# NOTE: In order to call/use this file...
+# 1> chmod u+x add03.sh (make it execuable)
+# 2> ./add03 5 6 # 11 (call it by using 2 arguments to be added up)
