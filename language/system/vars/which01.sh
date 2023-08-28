7339a26811d18bfa0bee2144a065c1d74b67ce3d
Paul Ramnora <pramnora@yahoo.com>
2023-08-28 09:46:07 +0100

    Update which01.sh
    Added some further examples of how to use the which command; as well as, added comments as to what that search displays on screen.

--- language/system/vars/which01.sh
+++ language/system/vars/which01.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash  
 
-which bash  
+# The which command can be used to search for where on the system an item is to be found...  
+
+which bash  # /usr/bin/bash
+which $BASH # /bin/bash
+which python3 # /usr/bin/python3
 
