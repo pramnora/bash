103b3f681990f64a98c1a459efc92ef93e352c61
Paul Ramnora <pramnora@yahoo.com>
2023-08-28 10:14:41 +0100

    Create man01.sh
    Created file: [man01.sh] Giving a simple explanation of how to use the built-in manual help: man command

---
+++ language/system/help/man/man01.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash  
+
+# The Linus manual can be accessed within the system...by, simply, typing in...
+# man command
+# ...which will find help relating to that particular command.
+# For example...
+
+man ls # displays help relating to the 'ls' or list command
