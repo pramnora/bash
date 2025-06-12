6ce37549a7b6a31ccc75823648a7c4a6aa19aa1e
Paul Ramnora <pramnora@yahoo.com>
2025-06-12 21:00:33 +0100

    Update file01.sh
    Added some line separators as code comments.

--- file01.sh
+++ file01.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+# the above line determines where the 'bash' program lies...
+# inside of the users Linux system
+
+# --------------------------------------------------------
+
 # Create file...
+
 touch fileName1.txt
 
-# List all folders/files in current directory...
+# --------------------------------------------------------
+
+# List all folders/files in current directory...;
+# including the newly 'created' file above.
+
 ls
 
+# --------------------------------------------------------
+
 # Remove file...
+
 rm fileName1.txt
 
-# List all folders/files in current directory...
+# --------------------------------------------------------
+
+# List all folders/files in current directory...;
+# including the newly 'removed' file above.
+
 ls
