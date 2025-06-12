80434f197fd57920f7be5f163fe658a7ddb19a82
Paul Ramnora <pramnora@yahoo.com>
2025-06-12 21:30:36 +0100

    Update list-storage.sh
    Added comments

--- list-storage.sh
+++ list-storage.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+
+# The above line indicates where inside of the users Linux system
+# the 'bash' program can be found
+
+# ---------------------------------------------------------------------------
+
+# This example program...
+# shows 3 different versions of how to use the 'ls' command
+
+# ---------------------------------------------------------------------------
+
+# List folders/files inside of current directory...
+
 ls
+
+# ---------------------------------------------------------------------------
+
+# List using long format folders/files inside of current directory...
+
 ls -l
+
+# ---------------------------------------------------------------------------
+
+# List 'all' folders/files...
+# including 'hidden' folders/files...which are prefixed using: .folderName/or, .fileName
+# too, using long format...inside of current directory... 
+
 ls -al
