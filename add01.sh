e90f36d46b7916c3d410212e32463292270e1fa3
Paul Ramnora <pramnora@yahoo.com>
2025-06-12 21:06:06 +0100

    Update add01.sh
    Added code comments...

--- add01.sh
+++ add01.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo $((1+1))
+
+# The above line of code is used to indicate where inside of the users Linux system
+# the 'bash' program is located.
+
+# ---------------------------------------------------------------------------------
+
+# The code below will print out the value of the expression sum: 1+1
+
+echo $((1+1)) # the output is: 2
