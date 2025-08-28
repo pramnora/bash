d8eae4813a10a4316243a7cff8234d3cc05b43ce
Paul Ramnora <pramnora@yahoo.com>
2025-08-28 17:07:02 +0100

    Update for-files-1.sh
    Added a vertical line break

--- language/looping/for/for-files-1.sh
+++ language/looping/for/for-files-1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This program will list out the contents of all [.txt] files...
+
 # it assumes there is, at least, one text file [.txt] 
 # inside of the current folder directory... 
 
