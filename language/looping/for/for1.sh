8e16a041f88547ddfaf2cd7972744a05d81a295d
Paul Ramnora <pramnora@yahoo.com>
2025-08-28 14:32:58 +0100

    Update for1.sh
    Added output comments...

--- language/looping/for/for1.sh
+++ language/looping/for/for1.sh
@@ -10,8 +10,20 @@ for ((num=1 ; num<maxNo ; num++)); do
  echo $num
 done
 
-# count down from n- to 1...
+# count down from n-1 to 1...
 
 for ((num=maxNo ; num>=1 ; num--)); do
  echo $num
 done
+
+# output...
+
+#1
+#2
+#3
+#4
+#5
+#4
+#3
+#2
+#1
