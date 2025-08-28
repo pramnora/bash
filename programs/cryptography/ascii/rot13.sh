6501d2e2f11da13ac36ff29f47166b57a42f6c48
Paul Ramnora <pramnora@yahoo.com>
2025-08-28 13:08:59 +0100

    Create rot13.sh
    Created file: [rot13.sh] A simple encoder/decoder...famously known as being called: rot13/or, rotate 13.

---
+++ programs/cryptography/ascii/rot13.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Encoder/Decoder alphabet(ROT13):"
+echo "-----"
+echo {a..m}
+echo {n..z}
+echo "-----"
+echo "USER INSTRUCTIONS"
+echo "1. Take a word/(or, phrase) such as: 'cab'."
+echo
+echo "2. You use the letter either above/or, below to encode..."
+echo "c = p"
+echo "a = n"
+echo "b = o"
+echo "...thus, plaintext word: 'cab' becomes 'pno'."
+echo
+echo "3. You use the letter either above/or, below to decode..."
+echo "p = c"
+echo "n = a"
+echo "b = o"
+echo "...thus, encoded text: 'pno' becomes 'cab'."
