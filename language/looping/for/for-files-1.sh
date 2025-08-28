01a0d61f12d353398c1047dc913bcba8e1846207
Paul Ramnora <pramnora@yahoo.com>
2025-08-28 17:07:42 +0100

    Update for-files-1.sh
    Added 2 horizontal line comments.

--- language/looping/for/for-files-1.sh
+++ language/looping/for/for-files-1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+# -----
 # This program will list out the contents of all [.txt] files...
 
 # it assumes there is, at least, one text file [.txt] 
 # inside of the current folder directory... 
-
+# -----
 for file in *.txt; do
  cat $file
 done
