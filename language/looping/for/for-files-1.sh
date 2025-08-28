8bd1c27b93962c938a70177dc570f1110ed70f1a
Paul Ramnora <pramnora@yahoo.com>
2025-08-28 17:08:36 +0100

    Update for-files-1.sh
    Added *

--- language/looping/for/for-files-1.sh
+++ language/looping/for/for-files-1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # -----
-# This program will list out the contents of all [.txt] files...
+# This program will list out the contents of all: [*.txt] files...
 
-# it assumes there is, at least, one text file [.txt] 
+# it assumes there is, at least, one text file: [.txt] 
 # inside of the current folder directory... 
 # -----
 for file in *.txt; do
