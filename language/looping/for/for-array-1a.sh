de12ead30203922ceef33382e6468f507e9794ca
Paul Ramnora <pramnora@yahoo.com>
2025-08-28 15:45:10 +0100

    Create for-array-1a.sh
    Created file: [for-array-1a.sh] Using a for loop to display multiple text string contents.

---
+++ language/looping/for/for-array-1a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# use for loop to display text string contents...
+
+for item in apple banana cherry; do
+ echo "I like $item"
+done
+
+# output...
+
+# I like apple
+# I like banana
+# I like cherry
