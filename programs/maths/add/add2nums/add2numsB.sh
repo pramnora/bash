50ac1f9bd5cc8874ef46ec21bacdcbf69a9f4274
Paul Ramnora <pramnora@yahoo.com>
2026-07-09 19:10:24 +0100

    Clarify backticks usage in addition script
    Updated echo statement to clarify the use of backticks.

--- programs/maths/add/add2nums/add2numsB.sh
+++ programs/maths/add/add2nums/add2numsB.sh
@@ -11,7 +11,10 @@ echo "Enter 2 numbers to add:"
 read num1
 read num2
 
-echo `expr $num1 + $num2` # NOTE: The use of a pair of back ticks: ``
+# NOTE: The use of a pair of back ticks: ``;
+# to surround the sum expression.
+
+echo The answer is: `expr $num1 + $num2` 
 
 # output...
 
