c958a3f52b8fa3c1c8f93a1d5f61b11a7e91c482
Paul Ramnora <pramnora@yahoo.com>
2026-07-09 19:06:21 +0100

    Create add2numsB.sh for adding two numbers
    Added a script to add two numbers with modified output.

---
+++ programs/maths/add/add2nums/add2numsB.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This code borrowed from:
+# https://www.youtube.com/shorts/wNUf3jEXS6E
+# -----------------------------------------------
+# NOTE: I've slightly modified the output line,
+# by including the text: The answer is:
+
+echo "Enter 2 numbers to add:"
+
+read num1
+read num2
+
+echo `expr $num1 + $num2`
+
+# output...
+
+# Enter 2 numbers to add:
+# 1
+# 2
+# The answer is: 3
+
+
