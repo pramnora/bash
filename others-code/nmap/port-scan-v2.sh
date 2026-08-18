da42dedda851a1d91de146ee205b7c9f65047a9d
Paul Ramnora <pramnora@yahoo.com>
2026-08-18 05:53:22 +0100

    Add specific port scanning for ports 20 and 21

--- others-code/nmap/port-scan-v2.sh
+++ others-code/nmap/port-scan-v2.sh
@@ -5,6 +5,7 @@
 # > bash port-scan-v2.sh URLaddress
 # ...then, immediately, type in the URL address you wish to scan.
 # ---------------------------------------------------------------
+echo "--Specific ports: 20,21--"
 sudo nmap -sS -p 20,21 $1
 echo "--OS Scan starts now--"
 sudo nmap -O $1
