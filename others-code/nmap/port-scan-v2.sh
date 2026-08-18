e2feaa280d6155df95e56a4be7afc409f2d26472
Paul Ramnora <pramnora@yahoo.com>
2026-08-18 05:45:41 +0100

    Create port-scan-v2.sh for nmap scanning
    Added a script for performing a port scan using nmap.

---
+++ others-code/nmap/port-scan-v2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ---------------------------------------------------------------
+# This is my own adaptation of the code:
+# You run the file using:
+# > bash port-scan-v2.sh URLaddress
+# ...then, immediately, type in the URL address you wish to scan.
+# ---------------------------------------------------------------
+sudo nmap -sS -p 20,21 $1
+echo "--OS Scan starts now--"
+sudo nmap -O $1
